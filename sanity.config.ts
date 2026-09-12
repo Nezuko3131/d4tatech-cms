27e2996394aecad3965948edcafe09915ec6d5d3
Nezuko3131 <gelhenescarfuller31@gmail.com>
2026-09-11 20:15:51 -0400

    Fix: Add basePath and structure tool config

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,18 +6,22 @@ import { schemaTypes } from './sanity/schemas'
 export default defineConfig({
   name: 'd4tatech-studio',
   title: 'D4taTech CMS',
+  basePath: '/studio',
 
   projectId: '330d9rwq',
   dataset: 'production',
 
   plugins: [
     structureTool({
+      name: 'myStructure',
+      title: 'Content',
       structure: (S) =>
         S.list()
           .title('Contenido')
           .items([
             S.listItem()
               .title('Sobre Mi')
+              .id('sobreMi')
               .child(
                 S.document()
                   .schemaType('sobreMi')
